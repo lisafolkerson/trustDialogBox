2349d3f2182045f806e7e2f2b46c963385ff2a55
Lisa Folkerson <lisafolkerson@gmail.com>
2015-02-26 15:25:33 -0500

    now gulp actually working

--- Li009-dontShootTheMessenger/assets/Gulpfile.js
+++ Li009-dontShootTheMessenger/assets/Gulpfile.js
@@ -1,1 +1,13 @@
  var gulp = require('gulp');
+ var sass = require( 'gulp-sass' );
+
+
+ gulp.task( 'styles', function(){
+ 	gulp.src( 'styles.scss' )
+ 	.pipe(sass())
+ 	.pipe(gulp.dest( './css' ))
+ }); // end gulp.task(styles)
+
+ gulp.task( 'watch', function(){
+ 	gulp.watch( '*.scss' [ 'styles' ]);
+ }); //end gulp.task(watch)
