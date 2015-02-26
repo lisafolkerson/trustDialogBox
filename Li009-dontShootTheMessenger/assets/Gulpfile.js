aefa0812773de37b4425e671284838dafbbf8b9a
Lisa Folkerson <lisafolkerson@gmail.com>
2015-02-26 15:47:59 -0500

    updated gitignore

--- Li009-dontShootTheMessenger/assets/Gulpfile.js
+++ Li009-dontShootTheMessenger/assets/Gulpfile.js
@@ -3,11 +3,12 @@
 
 
  gulp.task( 'styles', function(){
- 	gulp.src( 'styles.scss' )
+ 	gulp.src( './styles.scss' )
  	.pipe(sass())
  	.pipe(gulp.dest( './css' ))
  }); // end gulp.task(styles)
 
- gulp.task( 'watch', function(){
- 	gulp.watch( '*.scss' [ 'styles' ]);
+ 
+ gulp.task( 'watch', function(){ 	
+	gulp.watch( './styles.scss', [ 'styles' ]);
  }); //end gulp.task(watch)
