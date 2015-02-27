dec99ee64199deef15026ef35c99ef0e7ba02929
Lisa Folkerson <lisafolkerson@gmail.com>
2015-02-26 21:44:18 -0500

    about to make a branch abandoning the .dialog method

--- Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
+++ Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
@@ -1,9 +1,14 @@
 $.fn.trustDialogBox = function() {
 	// style dialog box for alert, prompt, confirm
-	$( this ).on('click',function () {
+	$( this ).on( 'click', function () {
+		// e.preventDefault();
 		$( this ).dialog({
-			dialogClass : 'noClose',
 			modal : true,
+			show : { 
+			  	effect : 'fadeIn', 
+			  	duration : 800 
+			  }, // end show object
+			title : 'Close This',  
 			buttons : [
 				{
 					text : "nanana",
@@ -13,21 +18,21 @@ $.fn.trustDialogBox = function() {
 				} // end nanana text object
 			] // end buttons array
 		}); // end this.dialog call
+	}); // end this click function();
+
+	$( '.scaryBox' ).dialog({
+	  dialogClass : 'alert'
+	  show : { 
+	  	effect : 'blind', 
+	  	duration : 800 
+	  } // end show object
+	}); // end .dog();
+
+	$( '.yesBox' ).dialog({
+	  dialogClass: 'prompt'
 	});
 
-	// $( '.scaryBox' ).dialog({
-	//   dialogClass : 'alert'
-	//   show : { 
-	//   	effect : 'blind', 
-	//   	duration : 800 
-	//   } // end show object
-	// }); // end .dog();
-
-	// $( '.yesBox' ).dialog({
-	//   dialogClass: 'prompt'
-	// });
-
-	// $( '.promptBox' ).dialog({
-	//   dialogClass: 'confirm'
-	// });
+	$( '.promptBox' ).dialog({
+	  dialogClass: 'confirm'
+	});
 } // end $.fn.trustDialogBox
