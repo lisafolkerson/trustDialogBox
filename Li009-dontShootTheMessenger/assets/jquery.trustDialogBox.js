7ff594297d02512601f2c5eaeb3d2f692d38f0a7
Lisa Folkerson <lisafolkerson@gmail.com>
2015-02-26 21:48:34 -0500

    here i come crapShoot

--- Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
+++ Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
@@ -1,38 +1,60 @@
 $.fn.trustDialogBox = function() {
 	// style dialog box for alert, prompt, confirm
 	$( this ).on( 'click', function () {
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		// e.preventDefault();
-		$( this ).dialog({
-			modal : true,
-			show : { 
-			  	effect : 'fadeIn', 
-			  	duration : 800 
-			  }, // end show object
-			title : 'Close This',  
-			buttons : [
-				{
-					text : "nanana",
-					click : function() {
-						$(this).dialog( 'close' );
-					} // end click function()
-				} // end nanana text object
-			] // end buttons array
-		}); // end this.dialog call
-	}); // end this click function();
+	// 	$( this ).dialog({
+	// 		modal : true,
+	// 		show : { 
+	// 		  	effect : 'fadeIn', 
+	// 		  	duration : 800 
+	// 		  }, // end show object
+	// 		title : 'Close This',  
+	// 		buttons : [
+	// 			{
+	// 				text : "nanana",
+	// 				click : function() {
+	// 					$(this).dialog( 'close' );
+	// 				} // end click function()
+	// 			} // end nanana text object
+	// 		] // end buttons array
+	// 	}); // end this.dialog call
+	// }); // end this click function();
 
-	$( '.scaryBox' ).dialog({
-	  dialogClass : 'alert'
-	  show : { 
-	  	effect : 'blind', 
-	  	duration : 800 
-	  } // end show object
-	}); // end .dog();
+	// $( '.scaryBox' ).dialog({
+	//   dialogClass : 'alert'
+	//   show : { 
+	//   	effect : 'blind', 
+	//   	duration : 800 
+	//   } // end show object
+	// }); // end .dog();
 
-	$( '.yesBox' ).dialog({
-	  dialogClass: 'prompt'
-	});
+	// $( '.yesBox' ).dialog({
+	//   dialogClass: 'prompt'
+	// });
 
-	$( '.promptBox' ).dialog({
-	  dialogClass: 'confirm'
-	});
+	// $( '.promptBox' ).dialog({
+	//   dialogClass: 'confirm'
+	// });
 } // end $.fn.trustDialogBox
