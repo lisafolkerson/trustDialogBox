ee13067caa2b65b83698db492c3f294b282db6ff
Lisa Folkerson <lisafolkerson@gmail.com>
2015-03-01 19:39:36 -0500

    back on track after rocky afternoon

--- Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
+++ Li009-dontShootTheMessenger/assets/jquery.trustDialogBox.js
@@ -1,60 +1,36 @@
 $.fn.trustDialogBox = function() {
 	// style dialog box for alert, prompt, confirm
 	$( this ).on( 'click', function () {
-		
+			$( '.alertItself').addClass( 'open' );
+			$( '.dimening').addClass( 'open' );
 
+			var togglino = function() {
+				if( this.hasClass( 'open' )) {
+					$( '.alertItself' ).removeClass( 'open' );
+				} else {
+					mo.open();
+				};
+			};// end togglino
 
+			var close = function() {
+				( '.clickable' ).removeClass( 'open' );
+			};
 
+			var togglino = function() {
+				if(xx.hasClass( 'open' )) {
+					mo.close();
+				} else {
+					mo.open();
+				};
+			};
 
+			var setContent = function(content) {
+				xx.find( '.content' ).html(content);
+			};
 
+			var init = function() {
+				console.log( 'Init Called' );
+			};		
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		// e.preventDefault();
-	// 	$( this ).dialog({
-	// 		modal : true,
-	// 		show : { 
-	// 		  	effect : 'fadeIn', 
-	// 		  	duration : 800 
-	// 		  }, // end show object
-	// 		title : 'Close This',  
-	// 		buttons : [
-	// 			{
-	// 				text : "nanana",
-	// 				click : function() {
-	// 					$(this).dialog( 'close' );
-	// 				} // end click function()
-	// 			} // end nanana text object
-	// 		] // end buttons array
-	// 	}); // end this.dialog call
-	// }); // end this click function();
-
-	// $( '.scaryBox' ).dialog({
-	//   dialogClass : 'alert'
-	//   show : { 
-	//   	effect : 'blind', 
-	//   	duration : 800 
-	//   } // end show object
-	// }); // end .dog();
-
-	// $( '.yesBox' ).dialog({
-	//   dialogClass: 'prompt'
-	// });
-
-	// $( '.promptBox' ).dialog({
-	//   dialogClass: 'confirm'
-	// });
+		}); // end click function
 } // end $.fn.trustDialogBox
--- Li009-dontShootTheMessenger/assets/scripts.js
+++ Li009-dontShootTheMessenger/assets/scripts.js
@@ -1,12 +1,4 @@
 $(function() {
-	
 	$( '.clickable' ).trustDialogBox();
-
-	// $( '.scaryBox' ).on( 'click', function() {
-	// 	alert();
-	// });
-	// $( 'yesBox' ).on( 'click', 
-	// 	)
-	// $( 'promptBox' ).on( 'click', )
-
+	
 }); // end doc ready
